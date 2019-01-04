6ca800b31b3300c2ece1677ba284d675bc39d97e
kavya.pl <kavya.pl@gmail.com>
2019-01-04 21:22:50 +0530

    API Xtnddd

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const post = require("./post");
+const user = require("./user");
 app.use(cors());
 app.use(express.json());
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mydb")
+  .connect("mongodb://127.0.0.1:27017/mydbNew")
   .catch(err => console.log(err));
 app.get("/posts", async (req, res) => {
   console.log("hitting..");
@@ -57,6 +58,37 @@ app.delete("/posts/:id", (req, res) => {
     })
     .catch(err => {
       res.send({ msg: "Failed" });
+    });
+});
+
+app.post("/login", (req, res) => {
+  user
+    .find({ name: req.body.name, password: req.body.password })
+    .then(data => {
+      if (data.length > 0) {
+        res.send({ msg: data });
+      } else {
+        res.send({ msg: "User not found" });
+      }
+    })
+    .catch(err => {
+      res.send({ msg: err });
+    });
+});
+app.post("/createUser", (req, res) => {
+  const user_ = new user({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  user_
+    .save()
+    .then(result => {
+      res.send({ msg: result });
+    })
+    .catch(err => {
+      res.send({ error: err });
     });
 });
 app.listen(8900, () => {
